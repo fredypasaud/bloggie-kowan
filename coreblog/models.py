469bed1a1542cce950577ab2b5971ea6aa942650
Fredy Silitonga <fredy.silitonga@tokopedia.com>
2022-11-30 17:50:03 +0700

    initial push for core blog

--- coreblog/models.py
+++ coreblog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class BlogPost(models.Model):
+    #user
+    blog_id = models.AutoField(primary_key=True)
+    judul_blog = models.CharField(max_length=100)
+    blog_text = models.TextField()
+    post_time = models.DateTimeField(blank=True, null=True)
--- coreblog/views.py
+++ coreblog/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from datetime import datetime
+from .models import BlogPost
+from .forms import BlogPostForm
 
 # Create your views here.
+
+def create_blog(request):
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST)
+        if form.is_valid():
+            blog_post = BlogPost.objects.create(
+                judul_blog = form.cleaned_data.get('judul_blog'),
+                blog_post = form.cleaned_data.get('blog_text'),
+                post_time = datetime.now()
+            )
+            blog_post.save()
+    return redirect('/')
+
+def see_all_post_non_specific_user(request):
+    blog_post = BlogPost.objects.all()
+    for blog in blog_post:
+        context = {
+            'blog_author' : blog.user,
+            'blog_post': blog.blog_text,
+            'post_title': blog.judul_blog,
+            'post_time': blog.post_time
+        }
+    return render(request, 'blog_post.html', context)
+
+def see_all_post_specific_user(request, username):
+    blog_post = BlogPost.objects.filter(user = username)
+    for blog in blog_post:
+        context = {
+            'blog_author' : blog.user,
+            'blog_post': blog.blog_text,
+            'post_title': blog.judul_blog,
+            'post_time': blog.post_time
+        }
+    return render(request, 'blog_post.html', context)
+
+def see_detailed_post(request, id):
+    blog_post = BlogPost.objects.filter(pk = id)
+    context = {
+    'blog_author' : blog_post.user,
+    'blog_post': blog_post.blog_text,
+    'post_title': blog_post.judul_blog,
+    'post_time': blog_post.post_time}
+    return render(request, 'blog_detailed.html', context)
